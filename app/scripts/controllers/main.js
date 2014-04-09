62ea709459e1f336521f76c366189577b41ea427
David Duke <ouroborot@gmail.com>
2014-04-09 09:09:02 -0700

    Add Restangular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('stackerWebappApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, Restangular) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    Restangular.setBaseUrl('http://localhost:8081');
+    Restangular.all('users').getList().then(function(users){
+      $scope.users = users;
+    });
+    Restangular.all('tasks').getList().then(function(tasks){
+      $scope.tasks = tasks;
+    });
   });
