b1504d5e785a10f1242ea4ad6fc6fb5a7eeab1e8
David Duke <ouroborot@gmail.com>
2014-04-18 00:03:26 -0700

    Save task edits and creation
    - Create a user for the client and save it to a cookie
    - POST when new tasks are added
    - PUT when tasks are edited

--- app/scripts/controllers/taskList.js
+++ app/scripts/controllers/taskList.js
@@ -1,19 +1,56 @@
 'use strict';
-function TaskListCtrl($scope, Restangular) {
+function TaskListCtrl($scope, Restangular, $cookies) {
   $scope.oneAtATime = true;
 
+  var baseTasks = Restangular.all('tasks');
   $scope.add = function () {
     $scope.tasks.unshift(
       {
         Title: "New Task",
-        Description: ""
+        Description: "",
+        OwnerId: $cookies.userId
       }
     );
+    baseTasks.post($scope.tasks[0]).then(function(task){
+      $scope.tasks[0].Id = task.Id;
+    });
     $scope.tasks[0].isOpen = true;
   };
 
+  $scope.saveTitle = function (data, id) {
+    Restangular.one('tasks', id).put({Title: data});
+  };
+
+  $scope.saveDescription = function (data, id) {
+    Restangular.one('tasks', id).put({Description: data});
+  };
+
   Restangular.setBaseUrl('http://localhost:8081');
-  Restangular.all('tasks').getList().then(function(tasks){
-    $scope.tasks = tasks;
-  });
+  var userId = $cookies.userId;
+  if (typeof userId === 'undefined') {
+    var user = {FullName: 'New User'};
+    Restangular.all('users').post(user).then(function(user) {
+      $scope.loggedInUser = user;
+      $cookies.userId = $scope.loggedInUser.Id;
+      Restangular.one('users', $scope.loggedInUser.Id).getList('tasks').then(function(tasks){
+        $scope.tasks = tasks;
+      });
+    });
+  } else {
+    Restangular.one('users', userId).get().then(function(user) {
+      $scope.loggedInUser = user;
+      Restangular.one('users', $scope.loggedInUser.Id).getList('tasks').then(function(tasks){
+        $scope.tasks = tasks;
+      });
+    }, function() {
+      var user = {FullName: 'New User'};
+      Restangular.all('users').post(user).then(function(user) {
+        $scope.loggedInUser = user;
+        $cookies.userId = $scope.loggedInUser.Id;
+        Restangular.one('users', $scope.loggedInUser.Id).getList('tasks').then(function(tasks){
+          $scope.tasks = tasks;
+        });
+      });
+    });
+  }
 }
